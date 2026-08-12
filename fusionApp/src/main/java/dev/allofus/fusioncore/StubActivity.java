d04be19ae20b82dccf4dcc67df3c6a8055ce4843
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-12 15:37:02 +0600

    Update StubActivity.java

--- fusionApp/src/main/java/dev/allofus/fusioncore/StubActivity.java
+++ fusionApp/src/main/java/dev/allofus/fusioncore/StubActivity.java
@@ -1,20 +1,33 @@
 package dev.allofus.fusioncore;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class StubActivity extends android.app.Activity {
+/** Manifest-declared placeholder for dynamically loaded Unity activities. */
+public class StubActivity extends Activity {
+    private static final String TAG = "StubActivity";
 
     @Override
     protected void onCreate(Bundle bundle) {
-        super.onCreate(null);
-        Intent intent = new Intent(this, SelectorActivity.class);
-        startActivity(intent);
-    }
+        super.onCreate(bundle);
 
-    @Override
-    protected void onPause() {
-        // If we're about to be replaced by another activity, don't pause unnecessarily
-        super.onPause();
+        Intent current = getIntent();
+
+        if (current != null
+                && current.getBooleanExtra(
+                    InstrumentationHooks.EXTRA_IS_DYNAMIC_ACTIVITY, false)) {
+
+            Intent original = current.getParcelableExtra(
+                    InstrumentationHooks.EXTRA_ORIGINAL_INTENT);
+
+            if (original != null && original.getComponent() != null) {
+                Log.d(TAG, "Dynamic activity placeholder: "
+                        + original.getComponent().getClassName());
+            } else {
+                Log.e(TAG, "Dynamic activity has no valid original intent");
+            }
+        }
     }
 }

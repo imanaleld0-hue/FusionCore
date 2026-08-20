13b7fbe971e66993ee5ad040074bfd98c03fbffc
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-20 09:14:16 +0600

    Create BootstrapStage.java

---
+++ fusionApp/src/main/java/dev/allofus/fusioncore/BootstrapStage.java
@@ -0,0 +1,28 @@
+package dev.allofus.fusioncore.bootstrap;
+
+public enum BootstrapStage {
+
+    INITIALIZING,
+
+    CHECKING_GAME,
+
+    COPYING_DATA,
+
+    DETECTING_UNITY,
+
+    DOWNLOADING,
+
+    PREPARING_RUNTIME,
+
+    INITIALIZING_IL2CPP,
+
+    GENERATING_INTEROP,
+
+    LOADING_ASSEMBLIES,
+
+    STARTING_GAME,
+
+    READY,
+
+    ERROR
+}

87fa6ef755d443768b75fbca8ddedd572e9afa69
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-21 13:19:48 +0600

    Update build.gradle.kts

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.browser:browser:1.8.0")
     implementation("com.google.android.gms:play-services-auth:21.3.0")
-    implementation(platform("com.google.android.gms:play-services-bom:32.1.0"))
 }
 
 android {

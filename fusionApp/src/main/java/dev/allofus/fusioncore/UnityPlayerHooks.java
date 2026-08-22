6343863c18013c642b2991797baa5611af113c4f
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-22 22:05:54 +0600

    Update UnityPlayerHooks.java

--- fusionApp/src/main/java/dev/allofus/fusioncore/UnityPlayerHooks.java
+++ fusionApp/src/main/java/dev/allofus/fusioncore/UnityPlayerHooks.java
@@ -90,98 +90,105 @@ public class UnityPlayerHooks {
             Log.i(TAG, "Hooking constructor: " + constructor);
 
             Pine.hook(constructor, new MethodHook() {
+    private Activity activity;
 
-                @Override
-                public void beforeCall(Pine.CallFrame callFrame) {
-                    try {
-                        if (callFrame.args.length == 0 || callFrame.args[0] == null) {
-                            Log.w(TAG, "First argument is null, skipping hook");
-                            return;
-                        }
+    @Override
+    public void beforeCall(Pine.CallFrame callFrame) {
+        try {
+            if (callFrame.args.length == 0 ||
+                    !(callFrame.args[0] instanceof Activity)) {
+                Log.w(TAG, "First argument is not an Activity, skipping hook");
+                return;
+            }
 
-                        if (!(callFrame.args[0] instanceof Activity)) {
-                            Log.w(TAG, "First argument is not an Activity instance, skipping hook");
-                            return;
-                        }
+            activity = (Activity) callFrame.args[0];
 
-                        Activity originalActivity = (Activity) callFrame.args[0];
+            Log.i(TAG, "Constructor firing, context class: "
+                    + activity.getClass().getName());
 
-                        Log.i(TAG, "Constructor firing, context class: " + originalActivity.getClass().getName());
+            callFrame.args[0] =
+                    new CustomContextWrapper(
+                            gameContext,
+                            activity,
+                            activity
+                    );
 
-                        callFrame.setObjectExtra("activity", originalActivity);
+        } catch (Throwable e) {
+            Log.e(TAG, "Failed to wrap context!", e);
+        }
+    }
 
-                        callFrame.args[0] = new CustomContextWrapper(
-                                gameContext,
-                                originalActivity,
-                                originalActivity
+    @Override
+    public void afterCall(Pine.CallFrame callFrame) {
+        if (activity == null) {
+            return;
+        }
+
+        Activity targetActivity = activity;
+
+        
+        targetActivity.getWindow().getDecorView().post(() -> {
+            try {
+                Window window = targetActivity.getWindow();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window.setDecorFitsSystemWindows(false);
+
+                    WindowInsetsController controller =
+                            window.getInsetsController();
+
+                    if (controller != null) {
+                        controller.hide(
+                                WindowInsets.Type.statusBars()
+                                        | WindowInsets.Type.navigationBars()
                         );
 
-                    } catch (Throwable e) {
-                        Log.e(TAG, "Failed to wrap context!", e);
+                        controller.setSystemBarsBehavior(
+                                WindowInsetsController
+                                        .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                        );
                     }
+                } else {
+                    window.setFlags(
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN
+                    );
+
+                    window.getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    );
                 }
 
-                @Override
-                public void afterCall(Pine.CallFrame callFrame) {
-                    Activity targetActivity = (Activity) callFrame.getObjectExtra("activity");
+                Log.i(TAG, "Fullscreen applied successfully");
 
-                    if (targetActivity == null) {
-                        return;
-                    }
+            } catch (Throwable e) {
+                Log.e(TAG, "Failed to apply fullscreen", e);
+            }
+        });
 
-                    targetActivity.getWindow().getDecorView().post(() -> {
-                        try {
-                            Window window = targetActivity.getWindow();
+        for (Field field : activityFields) {
+            try {
+                Log.i(TAG, "Setting activity field: "
+                        + field.getName());
 
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                                window.setDecorFitsSystemWindows(false);
+                field.set(callFrame.thisObject, activity);
 
-                                WindowInsetsController controller = window.getInsetsController();
-
-                                if (controller != null) {
-                                    controller.hide(
-                                            WindowInsets.Type.statusBars()
-                                                    | WindowInsets.Type.navigationBars()
-                                    );
-
-                                    controller.setSystemBarsBehavior(
-                                            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                                    );
-                                }
-                            } else {
-                                window.setFlags(
-                                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                                        WindowManager.LayoutParams.FLAG_FULLSCREEN
-                                );
-
-                                window.getDecorView().setSystemUiVisibility(
-                                        View.SYSTEM_UI_FLAG_FULLSCREEN
-                                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                );
-                            }
-
-                            Log.i(TAG, "Fullscreen applied successfully");
-
-                        } catch (Throwable e) {
-                            Log.e(TAG, "Failed to apply fullscreen", e);
-                        }
-                    });
-
-                    if (targetActivityField != null) {
-                        try {
-                            Log.i(TAG, "Setting activity field: " + targetActivityField.getName());
-                            targetActivityField.set(callFrame.thisObject, targetActivity);
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException(
-                                    "Failed to set activity field: " + targetActivityField.getName(), e
-                            );
-                        }
-                    }
-                }
+            } catch (Throwable e) {
+                Log.e(
+                        TAG,
+                        "Failed to set activity field: "
+                                + field.getName(),
+                        e
+                );
+            }
+        }
+    }
+});
             });
         }
     }

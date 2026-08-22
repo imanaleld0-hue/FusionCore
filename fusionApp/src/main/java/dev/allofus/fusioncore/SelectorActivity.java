4127c0701a56523522858bf34cd7843a16013bbe
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-22 12:53:38 +0600

    Update SelectorActivity.java

--- fusionApp/src/main/java/dev/allofus/fusioncore/SelectorActivity.java
+++ fusionApp/src/main/java/dev/allofus/fusioncore/SelectorActivity.java
@@ -87,7 +87,35 @@ public class SelectorActivity extends AppCompatActivity {
         }
     }
 
+   private void enableImmersiveMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            );
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (ActivityBridge.handleGooglePlayMergeIntent(intent)) {
+            Log.i(TAG, "Auth intent обработан в onNewIntent");
+        }
+    }
+    
     @Override
     protected void onResume() {
         super.onResume();

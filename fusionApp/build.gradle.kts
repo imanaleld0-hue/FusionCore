c6ec0b16f2cd58f41be8757eaa0b1bc61c7951a4
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-28 15:59:29 +0600

    Update build.gradle.kts

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("io.github.hexhacking:xdl:2.3.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("androidx.preference:preference:1.2.1")
+    implementation("org.apache.commons:commons-compress:1.27.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso")

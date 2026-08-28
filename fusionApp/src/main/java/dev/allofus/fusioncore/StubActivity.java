9679eb8b3f173861284351cf300b1137fb2ce03c
imanaleld0-hue <imanaleld0@gmail.com>
2026-08-28 10:26:46 +0600

    fix: resolve build errors (StubActivity, MiniIDE findViewById, Intent import)

---
+++ fusionApp/src/main/java/dev/allofus/fusioncore/StubActivity.java
@@ -0,0 +1,12 @@
+package dev.allofus.fusioncore;
+
+import android.os.Bundle;
+import dev.allofus.fusioncore.ui.BaseFullscreenActivity;
+
+public class StubActivity extends BaseFullscreenActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Stub activity used by instrumentation hooks
+    }
+}
